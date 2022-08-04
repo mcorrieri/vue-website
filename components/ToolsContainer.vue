e57bb3bdf6b246907aff0e4374d3a97eb4febfe1
Michael <Mlcorrieri@gmail.com>
2022-08-04 13:23:28 -0400

    tooltips added to icons

--- components/ToolsContainer.vue
+++ components/ToolsContainer.vue
@@ -2,26 +2,147 @@
   <v-card class="mt-3 pb-5">
     <v-card-title>Tools I Use</v-card-title>
     <v-card-text class="text-center">
-      <v-icon class="mx-2 mb-2" x-large color="green">mdi-vuejs</v-icon>
-      <v-icon class="mx-2 mb-2" x-large color="blue">mdi-vuetify</v-icon>
-      <v-icon class="mx-2 mb-2" x-large color="green">mdi-nuxt</v-icon>
-      <v-icon class="mx-2 mb-2" x-large color="blue"
-        >mdi-microsoft-azure-devops</v-icon
-      >
-      <v-icon class="mx-2 mb-2" x-large color="yellow"
-        >mdi-language-javascript</v-icon
-      >
-      <v-icon class="mx-2 mb-2" x-large color="#90CAF9">mdi-react</v-icon>
-      <v-icon class="mx-2 mb-2" x-large color="red"
-        >mdi-language-ruby-on-rails</v-icon
-      >
-      <v-icon class="mx-2 mb-2" x-large color="red">mdi-language-ruby</v-icon>
-      <v-icon class="mx-2 mb-2" x-large>mdi-github</v-icon>
-      <v-icon class="mx-2 mb-2" x-large color="#FDD835">mdi-firebase</v-icon>
-      <v-icon class="mx-2 mb-2" x-large color="blue">mdi-material-ui</v-icon>
-      <v-icon class="mx-2 mb-2" x-large color="#EF6C00"
-        >mdi-language-html5</v-icon
-      >
+      <v-tooltip top>
+        <template v-slot:activator="{ on, attrs }">
+          <v-icon
+            v-on="on"
+            v-bind="attrs"
+            class="mx-2 mb-2"
+            x-large
+            color="green"
+            >mdi-vuejs</v-icon
+          >
+        </template>
+        <span>Vue</span>
+      </v-tooltip>
+      <v-tooltip top>
+        <template v-slot:activator="{ on, attrs }">
+          <v-icon
+            v-on="on"
+            v-bind="attrs"
+            class="mx-2 mb-2"
+            x-large
+            color="blue"
+            >mdi-vuetify</v-icon
+          >
+        </template>
+        <span>Vuetify</span>
+      </v-tooltip>
+      <v-tooltip top>
+        <template v-slot:activator="{ on, attrs }">
+          <v-icon
+            v-on="on"
+            v-bind="attrs"
+            class="mx-2 mb-2"
+            x-large
+            color="green"
+            >mdi-nuxt</v-icon
+          >
+        </template>
+        <span>Nuxt</span>
+      </v-tooltip>
+      <v-tooltip top>
+        <template v-slot:activator="{ on, attrs }">
+          <v-icon
+            v-on="on"
+            v-bind="attrs"
+            class="mx-2 mb-2"
+            x-large
+            color="blue"
+            >mdi-microsoft-azure-devops</v-icon
+          >
+        </template>
+        <span>Azure DevOps</span>
+      </v-tooltip>
+      <v-tooltip top>
+        <template v-slot:activator="{ on, attrs }">
+          <v-icon
+            v-on="on"
+            v-bind="attrs"
+            class="mx-2 mb-2"
+            x-large
+            color="yellow"
+            >mdi-language-javascript</v-icon
+          >
+        </template>
+        <span>JavaScript</span>
+      </v-tooltip>
+      <v-tooltip top>
+        <template v-slot:activator="{ on, attrs }">
+          <v-icon
+            v-on="on"
+            v-bind="attrs"
+            class="mx-2 mb-2"
+            x-large
+            color="#90CAF9"
+            >mdi-react</v-icon
+          >
+        </template>
+        <span>React</span>
+      </v-tooltip>
+      <v-tooltip top>
+        <template v-slot:activator="{ on, attrs }">
+          <v-icon v-on="on" v-bind="attrs" class="mx-2 mb-2" x-large color="red"
+            >mdi-language-ruby-on-rails</v-icon
+          >
+        </template>
+        <span>Ruby on Rails</span>
+      </v-tooltip>
+      <v-tooltip top>
+        <template v-slot:activator="{ on, attrs }">
+          <v-icon v-on="on" v-bind="attrs" class="mx-2 mb-2" x-large color="red"
+            >mdi-language-ruby</v-icon
+          >
+        </template>
+        <span>Ruby</span>
+      </v-tooltip>
+      <v-tooltip top>
+        <template v-slot:activator="{ on, attrs }">
+          <v-icon v-on="on" v-bind="attrs" class="mx-2 mb-2" x-large
+            >mdi-github</v-icon
+          >
+        </template>
+        <span>GitHub</span>
+      </v-tooltip>
+      <v-tooltip top>
+        <template v-slot:activator="{ on, attrs }">
+          <v-icon
+            v-on="on"
+            v-bind="attrs"
+            class="mx-2 mb-2"
+            x-large
+            color="#FDD835"
+            >mdi-firebase</v-icon
+          >
+        </template>
+        <span>Firebase</span>
+      </v-tooltip>
+      <v-tooltip top>
+        <template v-slot:activator="{ on, attrs }">
+          <v-icon
+            v-on="on"
+            v-bind="attrs"
+            class="mx-2 mb-2"
+            x-large
+            color="blue"
+            >mdi-material-ui</v-icon
+          >
+        </template>
+        <span>MUI</span>
+      </v-tooltip>
+      <v-tooltip top>
+        <template v-slot:activator="{ on, attrs }">
+          <v-icon
+            v-on="on"
+            v-bind="attrs"
+            class="mx-2 mb-2"
+            x-large
+            color="#EF6C00"
+            >mdi-language-html5</v-icon
+          >
+        </template>
+        <span>HTML</span>
+      </v-tooltip>
     </v-card-text>
   </v-card>
 </template>
