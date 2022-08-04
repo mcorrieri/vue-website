5a1bcb7af57c7a323a7952a4bef4520589dd7ee1
Michael <Mlcorrieri@gmail.com>
2022-08-04 13:38:25 -0400

    added tools to TCS role

--- components/ExperienceContainer.vue
+++ components/ExperienceContainer.vue
@@ -1,11 +1,14 @@
 <template>
   <v-card class="mt-3 pb-4">
     <v-card-title class="my-3">Experience</v-card-title>
+
     <v-card-subtitle class="text-h6"
-      >ASR Analytics - Developer I</v-card-subtitle
-    >
+      >ASR Analytics - Developer I
+    </v-card-subtitle>
+
     <v-card-text class="text-subtitle-1"> April 2022 - Present</v-card-text>
-    <v-card-text class="text-center text-h5 mb-3"
+
+    <v-card-text class="text-center text-h6"
       >Tools:
       <v-icon large color="green">mdi-vuejs</v-icon>
       <v-icon large color="blue">mdi-vuetify</v-icon>
@@ -14,19 +17,20 @@
     </v-card-text>
 
     <v-divider></v-divider>
-    <v-card-subtitle class="text-h6"
-      >The Coding Space - Coding Instructor</v-card-subtitle
-    >
-    <v-card-text class="text-subtitle-1">
-      November 2021 - April 2022</v-card-text
-    >
 
-    <!-- <v-card-text class="text-center text-h5 mb-3"
+    <v-card-subtitle class="text-h6"
+      >The Coding Space - Coding Instructor
+    </v-card-subtitle>
+
+    <v-card-text class="text-subtitle-1">
+      November 2021 - April 2022
+    </v-card-text>
+
+    <v-card-text class="text-center text-h6"
       >Tools:
       <v-icon large color="yellow">mdi-language-javascript</v-icon>
       <v-icon large color="#90CAF9">mdi-react</v-icon>
-      <v-icon large>mdi-github</v-icon>
-    </v-card-text> -->
+    </v-card-text>
   </v-card>
 </template>
 
